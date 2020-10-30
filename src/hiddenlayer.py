1f3e59cd77b185db025329a2c1ee65be9a07a44e
vincentbudianto <vincent.budianto@gmail.com>
2020-10-30 19:00:40 +0700

    + laporan final

--- src/hiddenlayer.py
+++ src/hiddenlayer.py
@@ -3,7 +3,7 @@ from scipy.special import softmax
 
 class HiddenLayer:
     def __init__(self, input_size = 4, hidden_size = 3, output_size = 4):
-        self.name = "HiddenLayer"
+        self.name = 'HiddenLayer'
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -15,7 +15,7 @@ class HiddenLayer:
         self.output_activation = 'sigmoid'
 
         self.reset_history()
-    
+
     def initialize_weight(self, weight_type = -1, hidden_weight = None, output_weight = None, hidden_transition_weight = None):
         if weight_type == -1:
             # Random
@@ -39,7 +39,7 @@ class HiddenLayer:
             self.hidden_weight = hidden_weight
             self.output_weight = output_weight
             self.hidden_transition_weight = hidden_transition_weight
-    
+
     def initialize_bias(self, bias_type = -1, hidden_bias = None, output_bias = None):
         if bias_type == -1:
             # Random
@@ -59,12 +59,12 @@ class HiddenLayer:
             # Explicit
             self.hidden_bias = hidden_bias
             self.output_bias = output_bias
-    
+
     def reset_history(self):
         self.input_history = []
         self.hidden_history = []
         self.output_history = []
-    
+
     def get_history(self, layer_part = 'input', iteration = None):
         if layer_part == 'input':
             if iteration is None:
@@ -73,7 +73,7 @@ class HiddenLayer:
                 if len(self.input_history) > iteration and iteration >= 0:
                     return self.input_history[iteration]
                 else:
-                    return "ERROR. Iteration number is not correct"
+                    return 'ERROR. Iteration number is not correct'
         elif layer_part == 'hidden':
             if iteration is None:
                 return self.hidden_history
@@ -81,7 +81,7 @@ class HiddenLayer:
                 if len(self.hidden_history) > iteration and iteration >= 0:
                     return self.hidden_history[iteration]
                 else:
-                    return "ERROR. Iteration number is not correct"
+                    return 'ERROR. Iteration number is not correct'
         elif layer_part == 'output':
             if iteration is None:
                 return self.output_history
@@ -89,51 +89,51 @@ class HiddenLayer:
                 if len(self.output_history) > iteration and iteration >= 0:
                     return self.output_history[iteration]
                 else:
-                    return "ERROR. Iteration number is not correct"
+                    return 'ERROR. Iteration number is not correct'
 
     def forward_propagation_sequences(self, sequences, record_history = False):
         self.reset_history()
         self.previous_hidden = np.zeros(self.hidden_size)
         for sequence in sequences:
             self.forward_propagation(sequence, record_history)
-    
+
     def forward_propagation(self, input_features, record_history = False):
         if record_history:
             self.input_history.append(input_features)
-        
+
         sum_input = np.matmul(np.array(input_features), self.hidden_weight)
         sum_hidden = np.matmul(self.previous_hidden, self.hidden_transition_weight)
 
         hidden_total = sum_input + sum_hidden + self.hidden_bias
 
         self.previous_hidden = self.activation_function(hidden_total, self.hidden_activation)
-        
+
         if record_history:
             self.hidden_history.append(self.previous_hidden)
-        
+
         output_total = np.matmul(self.previous_hidden, self.output_weight) + self.output_bias
-        
+
         self.previous_output = self.activation_function(output_total, self.output_activation)
 
         if record_history:
             self.output_history.append(self.previous_output)
-    
+
 
     def activation_function(self, X, function_type):
-        if function_type == "sigmoid":
+        if function_type == 'sigmoid':
             X = np.clip(X, -500, 500)
             return 1.0/(1.0 + np.exp(-X))
-        elif function_type == "tanh":
+        elif function_type == 'tanh':
             return np.tanh(X)
-        elif function_type == "relu":
+        elif function_type == 'relu':
             return np.maximum(0,X)
-        elif function_type == "sigmoid":
-            return sigmoid(X)
+        elif function_type == 'softmax':
+            return softmax(X)
         else:
             return X
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     h1 = HiddenLayer()
     h1.initialize_weight(1)
     h1.initialize_bias(1)
